cdc5d136d3fa456146ba30eb33278e2c02100e3e
pjaegerl <paulocesarjaeger@gmial.com>
2024-08-16 21:09:41 +0200

    Dificultad FACIL

--- factorial-recursivo.js
+++ factorial-recursivo.js
@@ -3,14 +3,15 @@
  * de forma recursiva.
  */
 
+// UTILIZAREMOS UNA FUNCION RECURSIVA LA CUAL SE LLAMA A SI MISMA
 
 function factorial(n){
-    if( n == 0){
+    if(n == 0){
         return 1;
-    }else{
+    }else {
         return n * factorial(n-1);
-    }
-}
+    };
+};
 
 console.log(factorial(0));
 console.log(factorial(1));
