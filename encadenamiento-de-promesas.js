0cf3456e2eba35c8be41cbc04db3d2482feef10e
pjaegerl <paulocesarjaeger@gmial.com>
2024-08-28 01:13:23 +0200

    Dificultad FACIL

---
+++ encadenamiento-de-promesas.js
@@ -0,0 +1,33 @@
+// Crea una serie de funciones que devuelvan promesas y encadénalas para realizar una serie de operaciones secuenciales.
+
+function pasoUno(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('Paso 1 completo');
+            resolve();
+        }, 3000);
+    });
+};
+
+function pasoDos(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('paso 2 completo');
+            resolve();
+        }, 3000);
+    });
+};
+
+function pasoTres(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('Paso 3 completo');
+            resolve();
+        }, 3000);
+    });
+};
+
+pasoUno()
+    .then(pasoDos)
+    .then(pasoTres)
+    .then(() => console.log('Todos los pasos completados'));
