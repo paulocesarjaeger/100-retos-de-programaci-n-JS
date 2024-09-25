64f9dd468b32fb14685ea3242ea3e1dc2e4c675e
pjaegerl <paulocesarjaeger@gmial.com>
2024-09-25 15:26:44 +0200

    Dificultad FACIL

--- numero-primos-de-fibonacci.js
+++ numero-primos-de-fibonacci.js
@@ -14,7 +14,7 @@ function esPrimo(num){
     if(num <= 3) return true;
     if(num % 2 === 0 || num % 3 === 0) return false;
     for(let i = 5; i * i <= num; i += 6){
-        if(num % 1 === 0 || num % (i + 2) === 0) return false;
+        if(num % i === 0 || num % (i + 2) === 0) return false;
     }
     return true;
 }
